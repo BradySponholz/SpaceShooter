5b2f3c296e0475e62324df5f0ed42a0d9fb4026c
BradySponholz <152919263+BradySponholz@users.noreply.github.com>
2023-12-20 21:24:36 -0700

    Set player boundaries on x and y axis

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -9,14 +10,11 @@ public class Player : MonoBehaviour
     private float _speed = 3.5f;
 
 
-    // Start is called before the first frame update
     void Start()
     {
-        // take the current position = new position (0, 0, 0)
         transform.position = new Vector3(0, 0, 0);
     }
 
-    // Update is called once per frame
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
@@ -25,5 +23,23 @@ public class Player : MonoBehaviour
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 
         transform.Translate(direction * _speed * Time.deltaTime);
+
+        if (transform.position.y >= 1)
+        {
+            transform.position = new Vector3(transform.position.x, 1, 0);
+        }
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+
+        if (transform.position.x >= 9)
+        {
+            transform.position = new Vector3(9, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -9)
+        {
+            transform.position = new Vector3(-9, transform.position.y, 0);
+        }
     }
 }
