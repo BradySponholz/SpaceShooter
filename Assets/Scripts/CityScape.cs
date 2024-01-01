3b33bddd56bfdc9d130faea45f099419f7e059c5
BradySponholz <152919263+BradySponholz@users.noreply.github.com>
2023-12-31 19:31:58 -0700

    Replaced quad background with Grid City Scape, created script, set background to loop.

---
+++ Assets/Scripts/CityScape.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityScape : MonoBehaviour
+{
+    public float speed;
+    public float end;
+    public float start;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.down * speed * Time.smoothDeltaTime);
+
+        if (transform.position.y <= end)
+
+        {
+            Vector2 pos = new Vector2(0f, start);
+            transform.position = pos;
+        }
+    }
+}
